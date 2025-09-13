ed87d81fa5f9426f0ea58b8a22d3919dc4427b2d
gvvishal <727722euai068@skcet.ac.in>
2025-09-13 13:44:33 +0530

    Updated temp.java with new print statements

--- temp.java
+++ temp.java
@@ -2,5 +2,7 @@ class Main{
     public static void main(String[] args) {
         int n =10;
         System.out.println(n);
+        System.out.println("hi");
+        System.out.println("jdjdj");
     }
 }
